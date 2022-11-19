00fb0fb77b7c7072d26aa773232b0faba07fba81
dikiogres <dikiogres@gmail.com>
2022-11-19 11:01:51 +0700

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import style from './style'
 
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      <h1>Hello World</h1>
+      <div className={`${style.paddingX} ${style.flexCenter}`}>
+        <div className={`${style.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
+      <div className={`bg-primary ${style.flexCenter}`}>
+        <div className={`${style.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
     </div>
   )
 }
