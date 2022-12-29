c3ade1b167418489f1adadba1b42bebc39dd21aa
dikiogres <dikiogres@gmail.com>
2022-12-29 23:57:28 +0700

    feat: hover animate

--- src/components/GetStarted.tsx
+++ src/components/GetStarted.tsx
@@ -6,7 +6,7 @@ import { arrowUp } from '../assets'
 
 const GetStarted = () => {
   return (
-    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer shadow-cyan-500/50 hover:shadow-cyan-500/50 shadow-lg transition duration-500 hover:shadow-2xl hover:scale-110`}>
         <div className={`${styles.flexCenter} flex-col bg-primary h-[100%] w-[100%] rounded-full`}>
             <div className={`${styles.flexStart} flex-row`}>
                 <p className='font-poppins font-medium text-[18px] leading-[23px] mr-2'>
