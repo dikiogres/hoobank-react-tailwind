ee27374a6e924b26810ddcb1bf5a84889c409e4e
dikiogres <dikiogres@gmail.com>
2022-12-30 00:18:54 +0700

    feat: dynamic button component

--- src/components/Business.tsx
+++ src/components/Business.tsx
@@ -15,7 +15,7 @@ const Business = () => {
                 building credit, earning rewards and saving money. But with hundreds
                 of credit cards on the market.
             </p>
-            <Button />
+            <Button styles='mt-10' />
         </div>
     </section>
   )
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({styles}: {styles:any}) => {
   return (
-    <div>Button</div>
+    <div className={`rounded-lg shadow-cyan-500/50 hover:shadow-cyan-500/50 shadow-lg transition duration-500 hover:shadow-xl hover:scale-110 ${styles} `}>
+        <button type='button' className={`py-4 px-6 bg-blue-gradient rounded-lg font-poppins font-medium text-[18px] text-primary outline-none `}>
+            Get Started
+        </button>
+    </div>
   )
 }
 
