138ac05dd4682e97a2bea63c876ba448f5553349
dikiogres <dikiogres@gmail.com>
2022-12-29 22:22:28 +0700

    feat: structure skelly

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,29 @@ import style from './style'
 
 const App = () => {
   return (
-    <div className="bg-primary w-full overflow-hidden">
+    <div className="bg-primary w-full overflow-hidden  ">
       <div className={`${style.paddingX} ${style.flexCenter}`}>
         <div className={`${style.boxWidth}`}>
           Navbar
         </div>
       </div>
 
-      <div className={`bg-primary ${style.flexCenter}`}>
+      <div className={`bg-primary ${style.flexStart}`}>
         <div className={`${style.boxWidth}`}>
-          Navbar
+          Hero
+        </div>
+      </div>
+
+      <div className={`bg-primary ${style.paddingX} ${style.flexStart}`}>
+        <div className={`${style.boxWidth}`}>
+          Stats
+          Business 
+          Billing 
+          CardDeal 
+          Testimonials 
+          Clients 
+          CTA 
+          Footer
         </div>
       </div>
 
