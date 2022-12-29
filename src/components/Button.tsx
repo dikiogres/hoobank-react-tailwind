310fed5c95de4dfa17ccf5578f6b774c1d14cbbf
dikiogres <dikiogres@gmail.com>
2022-12-30 01:04:52 +0700

    feat: CTA section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = ({styles}: {styles:any}) => {
+const Button = ({styles}: {styles?:any}) => {
   return (
     <div className={`rounded-lg shadow-cyan-500/50 hover:shadow-cyan-500/50 shadow-lg transition duration-500 hover:shadow-xl hover:scale-110 ${styles} `}>
         <button type='button' className={`py-4 px-6 bg-blue-gradient rounded-lg font-poppins font-medium text-[18px] text-primary outline-none `}>
